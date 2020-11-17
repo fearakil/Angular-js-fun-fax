c53b5191af5d7c52b2e5a32a5a792df6b0ba4231
fearakil <apgomez15@gmail.com>
2020-11-17 00:14:24 -0600

    angular homework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'promises',
+    component: PromisesComponent
+  },
+  {
+    path: 'callbacks',
+    component: CallbacksComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'creator',
+    component: CreatorComponent
+  },
+  {
+    path: 'main',
+    component: MainPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromisesComponent,
+    CallbacksComponent,
+    MapComponent,
+    CreatorComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
